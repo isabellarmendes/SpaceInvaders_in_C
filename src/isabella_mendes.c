111ad64db2fc73164653599ef88667828f580d5a
isabella <isabelamendes827@gmail.com>
2026-07-02 21:18:25 -0300

    added professors notes

--- src/isabella_mendes.c
+++ src/isabella_mendes.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct tJogador{
+    int isAlive;
+    
+}
+
+typedef struct tAlien{
+    int isAlive;
+}
+
+typedef struct tTiro{
+
+}
+
+typedef struct tEstadoJogo{
+
+}
+
+typedef struct tPersonagens{
+
+}
+
+
+
+int main(int argc, char** argv) {
+
+
+    return (EXIT_SUCCESS);
+}
 
